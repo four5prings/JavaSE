1228653f29564a76d3e1f82acc9a3ddf60214280
four5prings <four5prings@163.com>
2023-08-15 21:34:20 +0800

    [Add] 重新开始

---
+++ JavaSE_/src/main/java/String_/ReStart.java
@@ -0,0 +1,14 @@
+package String_;
+
+/**
+ * @ClassName ReStart
+ * @Description
+ * @Author Four5prings
+ * @Date 2023/8/15 21:32
+ * @Version 1.0
+ */
+public class ReStart {
+    public static void main(String[] args) {
+        System.out.println("hello Java!");
+    }
+}
